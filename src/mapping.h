e56cb2cfad31c7bb2f324c2212164612cc84a07e
gerem <geremy.hutin@ens-lyon.fr>
2019-05-28 15:02:31 +0200

    _

--- src/mapping.h
+++ src/mapping.h
@@ -5,8 +5,11 @@
 #include <vector>
 
 class Mapping {
-
 public:
-    virtual void map(std::vector<Vec> &points2d, std::vector<Vec> &points3d);    
+    virtual void map(std::vector<Vec> &points2d, std::vector<Vec> &points3d)=0;
+};
 
+class PolarDiskMapping : public Mapping {
+public:
+    void map(std::vector<Vec> &points2d, std::vector<Vec> &points3d) override;
 };
